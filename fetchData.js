133b3f6c170966799ca8cc5159544802368ca873
Marc Gonzalez <marcgonzaleztarrio@gmail.com>
2022-09-22 15:31:51 +0200

    Dia 4

--- fetchData.js
+++ fetchData.js
@@ -1,34 +1,9 @@
-// function getData() {
-//   let title1, title2, title3;
-//   const body1 = document.querySelector("#body1");
-//   fetch("https://jsonplaceholder.typicode.com/posts")
-//     .then((result) => {
-//       return result.json();
-//     })
-//     .then((result) => {
-//       title1 = result[0];
-//       title2 = result[1];
-//       title3 = result[2];
-//       document.querySelector("#project1").innerText = title1.title.slice(0, 22);
-//       document.querySelector("#project2").innerText = title2.title.slice(0, 22);
-//       document.querySelector("#project3").innerText = title3.title.slice(0, 22);
-
-//       document.querySelector("#body1").innerText = title1.body;
-//       document.querySelector("#body2").innerText = title2.body;
-//       document.querySelector("#body3").innerText = title3.body;
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-// getData();
-
-const url = ["https://jsonplaceholder.typicode.com/posts?_limit=4"];
+const url = ["https://jsonplaceholder.typicode.com/posts?_limit=3"];
 function getData2() {
   fetch(url)
-    .then((response) => {
-      response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       let dataContainer = "";
       data.forEach((data) => {
         console.log(data);
@@ -44,15 +19,13 @@ function getData2() {
           <div class="text dataBody" id="body1">
           ${data.body}
           </div>
-          <a href="projects.html" class="learnMore">Learn more</a>
+          <a href="projects.html?${data.id}" class="learnMore">Learn more</a>
         </div>
       </article>
         `;
       });
+      document.querySelector(".projectsArticles").innerHTML = dataContainer;
     });
-  console.log("¨hello");
-  console.log(document.querySelector(".projectsArticles"));
-  // document.querySelector(".projectArticles").innerHTML = dataContainer;
 }
 
 getData2();
--- fetchProj.js
+++ fetchProj.js
@@ -1,25 +1,78 @@
-function getData() {
-  let title1, title2, title3;
-  const body1 = document.querySelector("#body1");
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((result) => {
-      return result.json();
-    })
-    .then((result) => {
-      title1 = result[0];
-      title2 = result[1];
-      title3 = result[2];
-      document.querySelector("#project1").innerText = title1.title;
-      document.querySelector("#project11").innerText = title1.title;
-      document.querySelector("#project2").innerText = title2.title;
-      document.querySelector("#project3").innerText = title3.title;
+let projNum = window.location.search.split("?")[1];
 
-      document.querySelector("#body1").innerText = title1.body;
-      document.querySelector("#body11").innerText = title1.body;
-      document.querySelector("#body2").innerText = title2.body;
-      document.querySelector("#body3").innerText = title3.body;
-    })
-    .catch((err) => console.log(err));
+console.log("projNum: " + projNum);
+const urlProject = [`https://jsonplaceholder.typicode.com/posts/${projNum}`];
+
+const urlOther = ["https://jsonplaceholder.typicode.com/posts?_limit=4"];
+
+function getData1() {
+  fetch(urlProject)
+    .then((response) => response.json())
+    .then((data) => {
+      let dataContainer2 = "";
+      dataContainer2 += `
+          <div class="section project wf-section">
+          <div class="container project">
+            <h1 class="project-page-title" id="project1">${data.title}</h1>
+            <div class="project-type-wrapper">
+              <div class="project-page-type">UI Design &amp; App Development</div>
+              <div class="project-completion">
+                <div class="project-completed">Completed on</div>
+                <div class="project-date">2/2/22</div>
+              </div>
+            </div>
+            <div class="project-image-wrapper">
+              <img
+                src="img/project-section/${data.id}.jpg"
+                loading="lazy"
+                alt=""
+                sizes="(max-width: 479px) 88vw, (max-width: 767px) 92vw, 94vw"
+                class="project-page-image"
+              />
+            </div>
+            <div class="project-detail w-richtext">
+              <p id="body1">
+                ${data.body}
+              </p>
+            </div>
+          </div>
+        </div>
+          `;
+      document.querySelector(".wf-section").innerHTML = dataContainer2;
+    });
 }
 
-getData();
+getData1();
+
+function getData2() {
+  fetch(urlOther)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let dataContainer = "";
+      data.forEach((data) => {
+        console.log(data);
+        if (data.id.toString() !== projNum) {
+          dataContainer += `
+        <article class="projectsArticle">
+        <img
+          src="img/project-section/${data.id}.jpg"
+          alt="imagen"
+          class="projectImg"
+        />
+        <div class="project_text">
+          <h3 class="dataTitle" id="project1">${data.title}</h3>
+          <div class="text dataBody" id="body1">
+          ${data.body}
+          </div>
+          <a href="projects.html?${data.id}" class="learnMore">Learn more</a>
+        </div>
+      </article>
+        `;
+        }
+      });
+      document.querySelector(".projectsArticles").innerHTML = dataContainer;
+    });
+}
+
+getData2();
